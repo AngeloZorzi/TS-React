17a09aa39358c6646ea652e23a511f028ad249fc
AngeloZorzi <astro.drg666@gmail.com>
2025-04-15 13:01:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <></>;
+}
+
+export default App;
